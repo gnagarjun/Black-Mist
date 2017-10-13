a112d020a404ac6b2d1f3af32946ced3d473c29c
ashacker <srividya.ramaswamy23@gmail.com>
2017-10-12 23:57:53 -0700

    inventory panel

--- Assets/Escape(Game Kit)/Scripts/clickToCollect.cs
+++ Assets/Escape(Game Kit)/Scripts/clickToCollect.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class clickToCollect : MonoBehaviour {
-
+	public static string nameofobj;
+	public GameObject objnametxt;
 	void OnMouseDown() {
+		nameofobj = gameObject.name;
 		Destroy(gameObject);
+		Destroy (objnametxt);
 	}
 }
---
+++ Assets/Escape(Game Kit)/Scripts/countDownTimer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class countDownTimer : MonoBehaviour
+{
+	//public float timeLeft = 5;
+
+	// Use this for initialization
+	void Start()
+	{
+		//StartCoroutine("LoseTime");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		//timeLeft -= Time.deltaTime;
+	}
+
+	//void OnGUI()
+	//{
+		//GUI.Label(Rect(10,10,100,40), ("Timer: " + timeLeft));
+	//}
+}
